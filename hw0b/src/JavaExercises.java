e6f7e6d6d9725f897782658c508885765750a8cd
Deng ZheWen <1920902706@qq.com>
2025-01-05 12:58:42 +0800

    commit hw0b

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,28 +1,60 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class JavaExercises {
 
-    /** Returns an array [1, 2, 3, 4, 5, 6] */
+    /**
+     * Returns an array [1, 2, 3, 4, 5, 6]
+     */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] array = new int[6];
+        for (int i = 0; i < 6; i++) {
+            array[i] = i + 1;
+        }
+        return array;
     }
 
-    /** Returns the order depending on the customer.
-     *  If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
-     *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
-     *  In any other case, return an empty String[] of size 3. */
+    /**
+     * Returns the order depending on the customer.
+     * If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
+     * If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
+     * In any other case, return an empty String[] of size 3.
+     */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        if (customer.equals("Ergun")) {
+            String[] array = new String[4];
+            array[0] = "beyti";
+            array[1] = "pizza";
+            array[2] = "hamburger";
+            array[3] = "tea";
+            return array;
+        } else if (customer.equals("Erik")) {
+            String[] array = new String[4];
+            array[0] = "sushi";
+            array[1] = "pasta";
+            array[2] = "avocado";
+            array[3] = "coffee";
+            return array;
+        } else {
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return max - min;
     }
 
     /**
@@ -38,8 +70,14 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if (x == 1) {
+            return list;
+        }else if (x % 2 == 0) {
+            return hailstoneHelper(x/2, list);
+        }else {
+            return hailstoneHelper(3*x + 1, list);
+        }
     }
 
 }
